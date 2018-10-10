80a518659eb7080fc6277bc1b9daf99d80cb1ab0
Andrew Quinn Merrell <Andrew.Merrell@gmail.com>
2018-10-09 21:23:55 -0600

    initial?

--- CampSiteMGR.sql
+++ CampSiteMGR.sql
@@ -1,11 +1,16 @@
+-----------------------------------------------------------Andrew's
 CREATE TABLE Reservation (
     ReservationID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    UserID INT NOT NULL,
+    UserID INT NOT NULL,            --FK
+    CampsiteID INT NOT NULL,        --FK
     CheckIn_Date DATE NOT NULL,
     CheckOut_Date DATE NOT NULL,
     Invoice_Total SMALLMONEY NOT NULL
 );
+
 CREATE TABLE Campsite_ReservedDates (
-    CampsiteID INT NOT NULL,
+    CampsiteID INT NOT NULL,        --FK
     DayReserved DATE NOT NULL
 );
+
+-----------------------------------------------------------
