12ef2b59a1250580b0a0cf9e4b68497b75f0bce1
Andrew Quinn Merrell <Andrew.Merrell@gmail.com>
2018-10-14 17:38:30 -0600

    I'd like to fix the @Current_Date thing

--- CampSiteMGR.sql
+++ CampSiteMGR.sql
@@ -82,7 +82,6 @@ INSERT INTO CampSites(CampgroundID, Longitude, Latitude, ImageURL,BaseCost) -- C
 	VALUES(2, 4438.725, 11051.687, 'https://ynpres1.xanterra.com/CGI-BIN/LANSAWEB?WEBEVENT+R616E0F8B07058601903A004+RES+ENG', 25.25);
 --Madison Sm Tent-Only Site
 
--- confused on capacity:                Capacity = how many people
 
 -- Campground 3
 INSERT INTO CampGrounds(CampgroundName, HostID, ParkingStalls, AddressID, Website, StandardCheckoutTime) 
@@ -115,16 +114,20 @@ CREATE TABLE Reservations (
 -- note, a date where a campsite is checked out, another may check in
 ---- Hmm, how do we account for that in counting the number of days utilized?
 
-SET IDENTITY_INSERT dbo.Reservation ON;
+SET IDENTITY_INSERT dbo.Reservations ON;
 DECLARE @CURRENT_DATE as DATE = Convert(DATE, GETDATE());
-INSERT Reservations VALUES ('1','1',@CURRENT_DATE, dateadd(day, 4, @CURRENT_DATE), 4 * 31, 1 );
-set @CURRENT_DATE = dateadd(day, 4, @CURRENT_DATE); -- add 4
-INSERT Reservations VALUES ('2','1',@CURRENT_DATE, dateadd(day, 4, @CURRENT_DATE), 4 * 31, 1 );
+INSERT Reservations (UserID, CampsiteID, CheckIn_Date, CheckOut_Date, Invoice_Total)
+ VALUES (1,1,@CURRENT_DATE, dateadd(day, 4, @CURRENT_DATE), 4 * 31);
+set @CURRENT_DATE = dateadd(day, 4, @CURRENT_DATE); -- add 
+INSERT Reservations (UserID, CampsiteID, CheckIn_Date, CheckOut_Date, Invoice_Total)
+VALUES (2,1,@CURRENT_DATE, dateadd(day, 4, @CURRENT_DATE), 4 * 31);
 set @CURRENT_DATE = Convert(DATE, GETDATE()); -- set it back
-INSERT Reservations VALUES ('2','2',@CURRENT_DATE, dateadd(day, 4, @CURRENT_DATE), 4 * 31, 1 );
+INSERT Reservations (UserID, CampsiteID, CheckIn_Date, CheckOut_Date, Invoice_Total)
+VALUES (2,2,@CURRENT_DATE, dateadd(day, 4, @CURRENT_DATE), 4 * 31);
 set @CURRENT_DATE = dateadd(day, 4, @CURRENT_DATE); -- add 4 again
-INSERT Reservations VALUES ('1','2',@CURRENT_DATE, dateadd(day, 4, @CURRENT_DATE), 4 * 31, 1 );
-SET IDENTITY_insert dbo.Reservation OFF;
+INSERT Reservations (UserID, CampsiteID, CheckIn_Date, CheckOut_Date, Invoice_Total)
+VALUES (1,2,@CURRENT_DATE, dateadd(day, 4, @CURRENT_DATE), 4 * 31);
+SET IDENTITY_insert dbo.Reservations OFF;
 
 /****************************************************  We decided against this "extraneous" table
 CREATE TABLE Campsite_ReservedDates (
